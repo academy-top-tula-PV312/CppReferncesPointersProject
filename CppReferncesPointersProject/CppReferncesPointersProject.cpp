b35dc4ada13fe9c8a06ca14b21488663a956f2c6
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-02-05 21:07:57 +0300

    05-02-24

--- CppReferncesPointersProject/CppReferncesPointersProject.cpp
+++ CppReferncesPointersProject/CppReferncesPointersProject.cpp
@@ -1,8 +1,16 @@
 ﻿#include <iostream>
 
-int func()
+int& func()
 {
-    return 100;
+    int x = 100;
+    int y = 200;
+    x += y;
+    return x;
+}
+
+int& value(int& x)
+{
+    return x;
 }
 
 void Swap(int& a, int& b)
@@ -12,54 +20,98 @@ void Swap(int& a, int& b)
     b = temp;
 }
 
+void newArray()
+{
+    int* arr = new int[100];
+    ///
+
+    delete[] arr;
+
+    arr = new int[50];
+
+}
+
 
 int main()
 {
-    int array[10];
+    //newArray();
 
-    std::cout << array << " " << &array[0] << "\n";
-    std::cout << func << "\n";
+    //int z = 100;
+    //value(z) = 200;
+    //std::cout << z << "\n";
 
-    int n{ 20 };
+
+
+    //int array[10];
+
+    //std::cout << array << " " << &array[0] << "\n";
+    //std::cout << func << "\n";
+
+    //int n{ 20 };
+    //
+    ///*std::cout << n << "\n";
+    //std::cout << &n << "\n";*/
+
+    ///*int* iptr = &n;
+    //iptr = array;*/
+
+    //const int& iref{n};
+    //std::cout << iref << "\n";
+    //n = 123;
+    //std::cout << n << "\n";
+
+    //int* iptr = &n;
+    //std::cout << &n << " " << iptr << "\n";
+    //std::cout << &iptr << "\n";
+    //std::cout << *iptr << "\n";
+
+    //*iptr = 123;
+    //std::cout << n << "\n";
+
+
+    //int x{ 30 };
+    //int y{ 50 };
+    //std::cout << x << " " << y << "\n";
+
+    //Swap(x, y);
+
+    //std::cout << x << " " << y << "\n";
+
+    //int size{ 10 };
+    //iptr = new int[size];
+
+    //iptr[0] = 10;
+    //for (int i{}; i < size; i++)
+    //    iptr[i] = (i + 1);
+
+    //for (int i{}; i < size; i++)
+    //    std::cout << *(iptr + i) << " ";
+    //std::cout << "\n";
+
+    //std::cout << iptr << " " << iptr + 2 << " " << iptr - 2 << "\n";
+
+
+    //int number{};
+    //int* pointer{};
+    //
+    //const int* const superconst{ &number };
+
+    /*int n;
+    float x = 12;
     
-    /*std::cout << n << "\n";
-    std::cout << &n << "\n";*/
+    int* iptr;
+    float* fptr;
 
-    /*int* iptr = &n;
-    iptr = array;*/
+    fptr = &x;
 
-    const int& iref{n};
-    std::cout << iref << "\n";
-    n = 123;
-    std::cout << n << "\n";
+    void* vptr = &x;
 
-    int* iptr = &n;
-    std::cout << &n << " " << iptr << "\n";
-    std::cout << &iptr << "\n";
-    std::cout << *iptr << "\n";
+    iptr = (int*)vptr;
+    std::cout << *iptr << "\n";*/
 
-    *iptr = 123;
-    std::cout << n << "\n";
+    int n{ 123 };
 
+    int* iptr{ &n };
 
-    int x{ 30 };
-    int y{ 50 };
-    std::cout << x << " " << y << "\n";
-
-    Swap(x, y);
-
-    std::cout << x << " " << y << "\n";
-
-    int size{ 10 };
-    iptr = new int[size];
-
-    iptr[0] = 10;
-    for (int i{}; i < size; i++)
-        iptr[i] = (i + 1);
-
-    for (int i{}; i < size; i++)
-        std::cout << *(iptr + i) << " ";
-    std::cout << "\n";
-
-    std::cout << iptr << " " << iptr + 2 << " " << iptr - 2 << "\n";
+    int** pptr{ &iptr };
 }
